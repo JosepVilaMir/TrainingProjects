c969a638eb54f5fa6ffcde91dfbb90b0aab852a6
Josep Vilaplana <josepvilamir@gmail.com>
2023-06-19 21:08:57 +0200

    Basic sight system for enemies implemented

---
+++ Unity Projects/SuperShooter/Assets/Scripts/Sight.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sight : MonoBehaviour
+{
+    [SerializeField] private float distance;
+    [SerializeField] private float angle;
+    [SerializeField] private LayerMask obstaclesLayers;
+    [SerializeField] private LayerMask objectsLayers;
+    [SerializeField] private Collider detectedObject;
+
+    private void Update()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, distance, objectsLayers);
+
+        detectedObject = null;
+        for(int i = 0; i < colliders.Length; i++)
+        {
+            Collider collider = colliders[i];
+
+            Vector3 directionToController = Vector3.Normalize(collider.bounds.center - transform.position);
+            float angleToCollider = Vector3.Angle(transform.forward,directionToController);
+
+            if(angleToCollider < angle)
+            {
+                if(!Physics.Linecast(transform.position, collider.bounds.center, obstaclesLayers))
+                {
+                    detectedObject = collider;
+                    break;
+                }
+            }
+        }
+    }
+}
